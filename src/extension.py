f172294d8fb627e743b30590f10e42a99568f1e9
Alberto de Leo <deleoa@tcd.ie>
2025-07-31 12:17:00 +0200

    Update extension.py

--- src/extension.py
+++ src/extension.py
@@ -1,58 +1,68 @@
 import logging
 import knime.extension as knext
+from bertopic import BERTopic
+import pandas as pd
 LOGGER = logging.getLogger(__name__)
 
+@knext.node(
+    name="Topic Extractor (BERTopic)", 
+    node_type=knext.NodeType.LEARNER, 
+    icon_path="../icons/icon.png", 
+    category="/",
+    description="Extract topics from documents using BERTopic.")
 
-@knext.node(name="My Template Node", node_type=knext.NodeType.LEARNER, icon_path="../icons/icon.png", category="/")
-@knext.input_table(name="Input Data", description="We read data from here")
-# @knext.input_table(name="Tutorial: Input Data 2", description="We also read data from here") ### Tutorial step 11: Uncomment to create a new input port
-@knext.output_table(name="Output Data", description="Whatever the node has produced")
+@knext.input_table(name="Document table", description="Data table with the document collection to analyze. Each row contains one document.")
+@knext.output_table(name="Document table with topics", description="The document collection with topic assignments and the probability for each document to belong to a certain topic.")
+@knext.output_table(name="Topic terms", description="The topic models with the terms and their weight per topic.")
 class TemplateNode:
-    """Short one-line description of the node.
-    Long description of the node.
-    Can be multiple lines.
+    """Use BERTopic to extract topics from documents. 
+    TODO Long description of the node.
     """
 
-    ### Tutorial step 10: Uncomment all of the following parameters and the 'is_numeric' method to create your first dialogue ###
-    # (Restart KAP and drag&drop the node again from the node repository to let changes take effect)
-    # some_param = knext.IntParameter("Some Int Parameter", "The answer to everything", 42, min_value=0)
+    # TODO Move to utils
+    def is_numeric(column):  # Filter columns visible in the column_param for numeric ones
+        return (
+            column.ktype == knext.double()
+            or column.ktype == knext.int32()
+            or column.ktype == knext.int64()
+        )
+    
+    def is_string(column):
+        return column.ktype == knext.string()
 
-    # another_param = knext.StringParameter("Some String parameter", "The classic placeholder", "foobar")
+    # Language in input documents
+    language_param = knext.StringParameter(label="Input language", description="", default_value="english")
 
-    # double_param = knext.DoubleParameter("Double Parameter", "Just for test purposes", 3.0)
+    # Calculate Probabilities
+    probabilities_param = knext.BoolParameter(label="Calculate Probabilities", description="Output probabilities", default_value=False)
+    
+    # Document column
+    document_column_param = knext.ColumnParameter(label="Document column", description="Documents from which topics should be extracted", port_index=0, column_filter=is_string)
 
-    # boolean_param = knext.BoolParameter("Boolean Parameter", "also just for testing", True)
+    # TODO Embedding model to use for topic extraction
+    embedding_model_param = knext.StringParameter(label="Embedding Model", description="The options to choose from.", default_value="SentenceTransformers", enum=["SentenceTransformers", "Flair", "Spacy", "Gensim"], is_advanced=True)
 
-    # column_param = knext.ColumnParameter()
-
-    # def is_numeric(column):  # Filter columns visible in the column_param for numeric ones
-    #     return (
-    #         column.ktype == knext.double()
-    #         or column.ktype == knext.int32()
-    #         or column.ktype == knext.int64()
-    #     )
-
-    # column_param = knext.ColumnParameter(label="label", description="description", port_index=0, column_filter=is_numeric)
-
-    def configure(self, configure_context, input_schema_1):
-    # def configure(self, configure_context, input_schema_1, input_schema_2):  ### Tutorial step 11: Uncomment to configure the new port (and comment out the previous configure header)
-        return input_schema_1
-        ### Tutorial step 12: Uncomment the following to adjust to the changes we do in this step in the execute method (and comment out the previous return statement)
-        # return input_schema_1.append(knext.Column(knext.double(), "column2"))
-        ### Tutorial step 13: Uncomment to set a warning for the configuration, which will be shown in the workflow
-        # configure_context.set_warning("This is a warning during configuration")
-
+    # def configure(self, configure_context, input_schema_1):
+    def configure(self, configure_context, input_schema_1):  ### Tutorial step 11: Uncomment to configure the new port (and comment out the previous configure header)
+        schema1 = input_schema_1.append(knext.Column(knext.int64(), "Topics"))
+        # return schema1
+        schema2 = knext.Schema([knext.int64(), knext.list_(inner_type=knext.string()), knext.list_(inner_type=knext.double())], ["Topic ID", "Term", "Weight"])
+        return schema1, schema2
  
-    def execute(self, exec_context, input_1):
-    # def execute(self, exec_context, input_1, input_2):  ### Tutorial step 11: Uncomment to accept the new port (and comment out the previous execute header)
-        return input_1
-        ### Tutorial step 12: Uncomment the following lines to work with the new port (and comment out the previous return statement)
-        # input_1_pandas = input_1.to_pandas() # Transform the input table to some processable format (pandas or pyarrow)
-        # input_2_pandas = input_2.to_pandas()
-        # input_1_pandas['column2'] = input_1_pandas['column1'] + input_2_pandas['column1']
-        # return knext.Table.from_pandas(input_1_pandas)
-        ### Tutorial step 13: Uncomment the following line to use the parameters from the configuration dialogue (and comment out the previous return statement)
-        # input_1_pandas['column2'] = input_1_pandas['column2'] + self.double_param
-        # LOGGER.warning(self.double_param) # Tutorial step 14: Logging some warning to the console
-        # exec_context.set_warning("This is a warning") # Tutorial step 14: Set a warning to be shown in the workflow
-        # return knext.Table.from_pandas(input_1_pandas) ### Tutorial step 13: Uncomment
+    # def execute(self, exec_context, input_1):
+    def execute(self, exec_context, input_1):  ### Tutorial step 11: Uncomment to accept the new port (and comment out the previous execute header)
+        input_1_pandas = input_1.to_pandas()
+        
+        # Compute the topics and output as new column
+        topic_model = BERTopic(language=self.language_param, calculate_probabilities=self.probabilities_param, nr_topics=20)
+        topics, probs = topic_model.fit_transform(input_1_pandas[self.document_column_param].to_list())
+        input_1_pandas['Topics'] = topics
+
+        # Compute the topics and get the most frequent words
+        # TODO I am very positive that this can be sped up and/or simplified
+        all_topics = topic_model.get_topics()
+        all_topics_df = pd.DataFrame(
+            [(key, list(t[0] for t in val), list(w[1] for w in val)) for key, val in all_topics.items()],
+            columns=['Topic ID', 'Term', 'Weight']
+        )
+        return knext.Table.from_pandas(input_1_pandas), knext.Table.from_pandas(all_topics_df)
